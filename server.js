c4519ecca2117e3fe34df85394b81d9f90866ca4
Asyrul Hafetzy <asyrulhafetzy@Asyruls-MacBook-Pro.local>
2020-05-01 02:54:43 +0100

    install compression and helmet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var index = require('./routes/index');
 var questions = require('./routes/questions.js');
@@ -12,6 +14,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+//Compress all routes
+app.use(compression());
+app.use(helmet());
 // Set Static folder
 app.use(express.static(path.join(__dirname, 'client')));
 
