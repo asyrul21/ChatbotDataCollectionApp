67cef7542090b954d16ef18bd7e8aeca5fdbfe5c
Asyrul Hafetzy <asyrulhafetzy@Asyruls-MacBook-Pro-2.local>
2020-12-11 10:34:23 +0800

    update connection string to mongo atlas

--- routes/questions.js
+++ routes/questions.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
-var mongooseToCsv = require('mongoose-to-csv')
-var fastCsv = require('fast-csv')
+var path = require("path");
+var mongooseToCsv = require("mongoose-to-csv");
+var fastCsv = require("fast-csv");
 
 // mongojs
 // var mongojs = require('mongojs');
@@ -10,79 +10,85 @@ var fastCsv = require('fast-csv')
 
 // mongoose
 // https://mongoosejs.com/docs/index.html
-var mongoose = require('mongoose');
-const fs = require('fs');
+var mongoose = require("mongoose");
+const fs = require("fs");
 
 // connect
-mongoose.connect('mongodb://asyrul:abc123@ds117931.mlab.com:17931/chatbot_training_data', { useNewUrlParser: true, useUnifiedTopology: true });
+// const connectionString =
+//   "mongodb://asyrul:abc123@ds117931.mlab.com:17931/chatbot_training_data";
+const connectionString =
+  "mongodb+srv://asyrul:abc123@chatbot-training-data.m0ymw.mongodb.net/chatbot_training_data?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // test connection
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connection to MongoDB established!');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connection to MongoDB established!");
 });
 
-var QuestionSchema = require('../models/Question');
+var QuestionSchema = require("../models/Question");
 // mongoose to csv
 QuestionSchema.plugin(mongooseToCsv, {
-    headers: 'Input Label',
-    constraints: {
-        'Input': 'input',
-        'Label': 'label'
-    }
+  headers: "Input Label",
+  constraints: {
+    Input: "input",
+    Label: "label",
+  },
 });
 // connect to db collection
-var Question = mongoose.model('Question', QuestionSchema);
+var Question = mongoose.model("Question", QuestionSchema);
 
-
-// The first argument is the singular name of the collection your model is for. 
-// ** Mongoose automatically looks for the plural, lowercased version of your model name. 
+// The first argument is the singular name of the collection your model is for.
+// ** Mongoose automatically looks for the plural, lowercased version of your model name.
 // ** Thus, for the example above, the model Question is for the questions collection in the database (mLab).
 
 // Get All questions
-router.get('/questions', function (req, res, next) {
-    Question.find(function (err, docs) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(docs);
-    });
+router.get("/questions", function (req, res, next) {
+  Question.find(function (err, docs) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(docs);
+  });
 });
 
 // insert questions
-router.post('/questions', function (req, res, next) {
-    var question = req.body;
-    const filename = 'training_data.csv';
+router.post("/questions", function (req, res, next) {
+  var question = req.body;
+  const filename = "training_data.csv";
 
-    // new Question instance
-    newQuestion = new Question({ input: question.input, label: question.label });
+  // new Question instance
+  newQuestion = new Question({ input: question.input, label: question.label });
 
-    // save question
-    newQuestion.save(function (err, question) {
-        if (err) {
-            res.send(err);
-        } else {
-            // save latest collection to csv
-            Question.findAndStreamCsv({})
-                .pipe(fs.createWriteStream(filename));
-            // return saved Question
-            res.json(question);
-        }
-    })
+  // save question
+  newQuestion.save(function (err, question) {
+    if (err) {
+      res.send(err);
+    } else {
+      // save latest collection to csv
+      Question.findAndStreamCsv({}).pipe(fs.createWriteStream(filename));
+      // return saved Question
+      res.json(question);
+    }
+  });
 });
 
 // download data as csv
-router.get('/getcsv', function (req, res, next) {
-    const filename = 'training_data.csv';
+router.get("/getcsv", function (req, res, next) {
+  const filename = "training_data.csv";
 
-    // find the file
-    filePath = path.join(__dirname, `../${filename}`)
+  // find the file
+  filePath = path.join(__dirname, `../${filename}`);
 
-    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-    res.setHeader('Content-Type', 'text/csv');
+  res.setHeader("Content-disposition", "attachment; filename=" + filename);
+  res.setHeader("Content-Type", "text/csv");
 
-    var filestream = fs.createReadStream(filePath);
-    filestream.pipe(res);
+  var filestream = fs.createReadStream(filePath);
+  filestream.pipe(res);
 });
 
 module.exports = router;
